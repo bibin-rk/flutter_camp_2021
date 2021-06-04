c414212dfdcc837ac85b27611836450b99473615
bibin-rk <bibinraj55555@gmail.com>
2021-06-04 14:32:13 +0530

    first commit

--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -6,42 +6,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-            child: Column(
-      children:[
-        Container(
-      padding: EdgeInsets.symmetric(vertical: 44, horizontal: 23),
-      width: double.infinity,
-      color: Color(0xff0B3D2E),
-      child: Row(
-        children: [
-          Column(
-            children: [
-              CircleAvatar(
-                  radius: 40,
-                  child: const SizedBox(
-                    width: 10,
-                  )),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("My Profile",
+            child: Column(children: [
+      Container(
+        padding: EdgeInsets.symmetric(vertical: 44, horizontal: 23),
+        width: double.infinity,
+        color: Color(0xff0B3D2E),
+        child: Row(
+          children: [
+            Column(
+              children: [
+                CircleAvatar(
+                    radius: 40,
+                    child: const SizedBox(
+                      width: 10,
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("My Profile",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            color: Colors.white)),
+                    Text(
+                      "bibin_rk",
                       style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 10,
                           fontWeight: FontWeight.w600,
-                          color: Colors.white)),
-                  Text(
-                    "bibin_rk",
-                    style: TextStyle(
-                        fontSize: 10,
-                        fontWeight: FontWeight.w600,
-                        color: Colors.white),
-                  )
-                ],
-              )
-            ],
-          ),
-        ],
-      ),
-    );
+                          color: Colors.white),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ],
+        ),
+      )
+    ])));
   }
 }
